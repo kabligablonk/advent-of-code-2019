586d5bda2e38197bfb97bb8363964b72e6c8e9da
Aeggne Miranda <aeggnemiranda@gmail.com>
2025-05-25 03:17:48 +0800

    Updated input reading logic

--- day 2/d2p1.py
+++ day 2/d2p1.py
@@ -1,5 +1,6 @@
-import csv
-
 with open("input.txt", "r") as input_list:
-    data = list(csv.reader(input_list))
-    print(data)
+    data = list(input_list)
+    list_input = []
+    for letters in data:
+        list_input.extend(letters.split(","))
+    print(list_input)
