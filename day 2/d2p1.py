b875fdebe9257c087ca71d36843df1b440583c1c
Aeggne Miranda <aeggnemiranda@gmail.com>
2025-05-25 19:47:43 +0800

    Updated d2p1

--- day 2/d2p1.py
+++ day 2/d2p1.py
@@ -1,6 +1,14 @@
+def plus(x, y):
+    z = input_list[x] + input_list[y]
+    print(z)
+
+
 with open("input.txt", "r") as input_list:
-    data = list(input_list)
     list_input = []
-    for letters in data:
+    for letters in input_list:
         list_input.extend(letters.split(","))
+    str_input_1 = str(input_list [1])
+    str_input_2 = str(input_list [2])
+    if list_input[0] == 1:
+        plus(str_input_1, str_input_2)
     print(list_input)
