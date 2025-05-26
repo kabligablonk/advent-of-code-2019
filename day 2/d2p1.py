2f1d9752d10e3e76fec5819aa6d9c00a49b4066c
Aeggne Miranda <aeggnemiranda@gmail.com>
2025-05-26 18:59:27 +0800

    Updated d2p1

--- day 2/d2p1.py
+++ day 2/d2p1.py
@@ -1,9 +1,15 @@
 def plus(x, y, a):
     z = int(separated_list_elements[x]) + int(separated_list_elements[y])
     separated_list_elements[a] = z
+    final_list_elements.extend(separated_list_elements[0:4])
+    del separated_list_elements[0:4]
+    print(z)
+    print(separated_list_elements)
+    print(final_list_elements)
 
 with open("input.txt", "r") as input_list:
     separated_list_elements = []
+    final_list_elements = []
     for elements in input_list:
         separated_list_elements.extend(elements.split(","))
     string_input_1 = int(separated_list_elements [1])
