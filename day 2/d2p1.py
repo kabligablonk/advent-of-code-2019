a1bc4b06b05008da268e651ea828c4c7dc7e4f1a
Aeggne Miranda <aeggnemiranda@gmail.com>
2025-05-29 16:13:12 +0800

    Updated d2p1

--- day 2/d2p1.py
+++ day 2/d2p1.py
@@ -15,10 +15,12 @@ with open("input.txt", "r") as input_list:
     final_list_elements = []
     for elements in input_list:
         separated_list_elements.extend(elements.split(","))
-    string_input_1 = int(separated_list_elements [1])
-    string_input_2 = int(separated_list_elements [2])
-    string_input_3 = int(separated_list_elements [3])
-    if int(separated_list_elements[0]) == 1:
-        plus(string_input_1, string_input_2, string_input_3)
-    if int(separated_list_elements[0]) == 2:
-        multiply(string_input_1, string_input_2, string_input_3)
+    while True:
+        opcode = int(separated_list_elements[0])
+        string_input_1 = int(separated_list_elements [1])
+        string_input_2 = int(separated_list_elements [2])
+        string_input_3 = int(separated_list_elements [3])
+        if opcode == 1:
+            plus(string_input_1, string_input_2, string_input_3)
+        if opcode == 2:
+            multiply(string_input_1, string_input_2, string_input_3)
